71d072e3f13dd5b51fc7e030359b7ad5fa4b1920
Jaroslaw Pasiak <wojapa@gmail.com>
2025-02-22 14:03:17 +0100

    add facebook messenger

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,39 +1,40 @@
 <template>
-    <div class="col">    
-        <div class="card rounded-4 bg-dark border-0 overflow-hidden" @click="open(link.iframe, link.link)" style="cursor: pointer;" :title="link.title">
-            <img height="150" :src="`${link.icon}`" class="card-img p-3">
-        </div>
+  <div class="col">
+    <div class="card rounded-4 bg-dark border-0 overflow-hidden" @click="open(link.iframe, link.link)"
+      style="cursor: pointer;" :title="link.title">
+      <img height="150" :src="`${link.icon}`" class="card-img p-3">
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 //Props
-    defineProps({
-        link: { type: Object , required: true }
-    });
-    
-//Emits
-    const emits = defineEmits(['cardClick']);
+defineProps({
+  link: { type: Object, required: true }
+});
 
-	const open = (iframe: boolean, link: string) => {
-        emits('cardClick', { iframe: iframe, link: `https://${link}` });
-	}
+//Emits
+const emits = defineEmits(['cardClick']);
+
+const open = (iframe: boolean, link: string) => {
+  emits('cardClick', { iframe: iframe, link: `https://${link}` });
+}
 </script>
 
 <style lang="css" scoped>
-    .card-img{
-        object-fit: contain;
-    }
+.card-img {
+  object-fit: contain;
+}
 
-    .card-img:hover{
-        scale: 1.2;
-        transition: ease-in 200ms;
-    }
+.card-img:hover {
+  scale: 1.2;
+  transition: ease-in 200ms;
+}
 
-    .card:hover{
-        scale: 1.05;
-        box-shadow: 0 0.5rem 1rem rgba(255, 0, 0, 0.5);
-        transition: ease-in 200ms;
-    }
+.card:hover {
+  scale: 1.05;
+  box-shadow: 0 0.5rem 1rem rgba(255, 0, 0, 0.5);
+  transition: ease-in 200ms;
+}
 </style>
--- src/components/MyServer.vue
+++ src/components/MyServer.vue
@@ -1,19 +1,19 @@
 <template>
-	<div class="d-grid container gap-2">
-		<template :key="link" v-for="link in links">
-			<div style="cursor: pointer; max-height: 10em;" class="card bg-dark" @click="open(link.link)">
-				<div class="card-body text-center">
-					<img :src="`${link.icon}`" style="max-height: 8em; max-width: -webkit-fill-available;" />
-				</div>
-			</div>
-		</template>
-	</div>
+  <div class="d-grid container gap-2">
+    <template :key="link" v-for="link in links">
+      <div style="cursor: pointer; max-height: 10em;" class="card bg-dark" @click="open(link.link)">
+        <div class="card-body text-center">
+          <img :src="`${link.icon}`" style="max-height: 8em; max-width: -webkit-fill-available;" />
+        </div>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
-  	import { links } from '@/scripts/shared';
+import { links } from '@/scripts/shared';
 
-	const open = (link: string) => {
-		window.open(`https://${link}`, '_blank');
-	}
+const open = (link: string) => {
+  window.open(`https://${link}`, '_blank');
+}
 </script>
