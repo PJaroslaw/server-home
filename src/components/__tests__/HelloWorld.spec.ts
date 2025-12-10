f1b39ced868e0f05d7327bf8b132ebc305cf561f
Jaroslaw Pasiak <wojapa@gmail.com>
2025-12-10 20:54:32 +0100

    Fix tests

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import MyServer from '../MyServer.vue'
+import App from '../../App.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(MyServer, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(App)
+    expect(wrapper.text()).toContain('Pjarek')
   })
 })
