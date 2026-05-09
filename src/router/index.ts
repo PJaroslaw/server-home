d5e9d455a988c81970743d640c03236e07cff3eb
Jarek Pasiak <wojapa@gmail.com>
2026-05-09 16:57:25 +0200

    Switch to real SPA experience (#173)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainComponent from '@/components/MainComponent.vue'
 import AdminComponent from '@/components/AdminComponent.vue'
 
-const isAdminSubdomain = /^admin\./i.test(window.location.hostname)
-const rootComponent = isAdminSubdomain ? AdminComponent : MainComponent
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: rootComponent
+      component: MainComponent
+    },
+    {
+      path: '/admin',
+      component: AdminComponent
     }
   ],
 })
